4e18eaf907c040b65b1e662d715a06c2e192e7bc
Kanilive1995 <ladavemoore@gmail.com>
2025-12-30 18:50:09 -0500

    Configuring
    Gitpages showing blank still

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/' // Must match your GitHub repo name
+  base: '/Portfolio/'  
 });
