383e3cbb996bbf4604ce9e24c6e713f742fbba67
Kanilive1995 <ladavemoore@gmail.com>
2025-12-30 18:16:35 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import './App.css' 
+
+
+function App() {
+  return (
+    <>
+      {/* HERO */}
+      <section className="hero">
+        <h1>
+          Kani <span>Moore-Singleton</span>
+        </h1>
+          <img src="/images/updatedheadshot.jpeg" alt="Headshot" className="Headshot" />
+        <p>
+          IT professional focused on Software/Application Development, Systems, and Security.
+        </p>
+        <p>      
+          I like my code, how I like my coffee: strong, efficient, and capable of keeping me up all night.
+        </p>
+
+        <div className="hero-buttons">
+          <a href="#projects">View Projects</a>
+          <a href="#contact" className="secondary">Contact Me</a>
+        </div>
+      </section>
+
+      {/* SKILLS */}
+      <section className="skills">
+        <h2>Skills</h2>
+
+        <div className="skill">
+          <p>JavaScript</p>
+          <div className="bar">
+            <div style={{ width: "85%" }} />
+          </div>
+        </div>
+
+        <div className="skill">
+          <p>Python</p>
+          <div className="bar">
+            <div style={{ width: "80%" }} />
+          </div>
+        </div>
+
+        <div className="skill">
+          <p>Linux</p>
+          <div className="bar">
+            <div style={{ width: "70%" }} />
+          </div>
+        </div>
+
+        <div className="skill">
+          <p>Splunk / SIEM</p>
+          <div className="bar">
+            <div style={{ width: "75%" }} />
+          </div>
+        </div>
+      </section>
+
+      {/* PROJECTS */}
+      <section className="projects" id="projects">
+        <h2>Projects</h2>
+
+        <div className="grid">
+          <div className="project-card">
+            <img src="/images/Dashboard.jpeg" alt="Splunk Dashboard" />
+            <div className="overlay">
+              <h3>Splunk Dashboard</h3>
+              <p>Firewall & network traffic monitoring</p>
+            </div>
+          </div>
+
+          <div className="project-card">
+            <img src="/images/Linux Installed.jpeg" alt="Kali Linux" />
+            <div className="overlay">
+              <h3>Kali Linux Lab</h3>
+              <p>UTM virtual lab on Apple Silicon</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* CONTACT */}
+      <section className="contact" id="contact">
+        <h2>Contact</h2>
+        <p> <img src="/images/Email.jpeg" alt="Email" className="email-logo" />Ladavemoore@gmail.com</p>
+        <p> <img src="/images/LinkedIn logo.jpeg" alt="LinkedIn" className="linkedin-logo" /> <a href="https://www.linkedin.com/in/kani-moore-singleton-883730137/">Kani Moore-Singleton</a></p>
+      </section>
+    </>
+  )
+}
+export default App
